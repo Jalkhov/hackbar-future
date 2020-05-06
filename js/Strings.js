e6bf018d7db4a421ba891fc56533a4999b11201d
Pedro Torcatt <pedrotorcattsoto@gmail.com>
2020-05-06 11:15:01 -0400

    Delete unnecessary default condition y switch

--- js/Strings.js
+++ js/Strings.js
@@ -1,4 +1,4 @@
-function SetStrings(id){
+function SetStrings(id) {
 	switch (id) {
 		/*------ADD SLASHES------*/
 		case "addslash":
@@ -68,8 +68,5 @@ function SetStrings(id){
 		case "lorem":
 			setSelectedText("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 			break;
-
-		default:
-			setSelectedText("www.facebook.com/Jalkhov");
 	}
 }
